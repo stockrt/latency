cc65ed042b206f6beb58afd5107db74a0d3bc6fc
Rogério Carvalho Schneider <stockrt@gmail.com>
2013-04-22 13:29:44 -0300

    PubSub modes

--- latency.rb
+++ latency.rb
@@ -14,20 +14,48 @@ require 'net/http'
 
 def main
   # Usage.
-  abort "#{$0} <domain> <port>" if ARGV.size != 2
+  abort "Usage: #{$0} <domain> <port> <mode>" if ARGV.size != 3
 
   # Params.
   domain = ARGV[0]
   port = ARGV[1].to_i
+  mode = ARGV[2]
 
-  # Subscriber loop.
+  case mode
+  when 'pub'
+    publisher(domain, port)
+  when 'sub'
+    subscriber(domain, port)
+  else
+    abort "Invalid mode: \"#{mode}\". Valid modes are: \"pub\" or \"sub\""
+  end
+end
+
+def publisher(domain, port)
+  buffer = ''
+  Net::HTTP.start(domain, port) do |http|
+    loop do
+      timestamp = Time.now.strftime('%s.%L')
+      message = "TS:#{timestamp}:"
+      http.request_post(URI.escape('/pub?id=latency'), message) do |response|
+        puts response.read_body
+      end
+    end
+  end
+end
+
+def subscriber(domain, port)
+  buffer = ''
   Net::HTTP.start(domain, port) do |http|
     http.request_get(URI.escape('/sub/latency')) do |response|
       response.read_body do |stream|
-        timestamp = stream.start_with?('TS:') ? stream.split(':')[1].to_i : nil
-        unless timestamp.nil?
-          latency = Time.now - Time.at(timestamp)
-          puts "Latency: #{latency}s"
+        buffer += stream
+        while line = buffer.slice!(/.+\r\n/)
+          timestamp = line.start_with?('TS:') ? line.split(':')[1].to_i : nil
+          unless timestamp.nil?
+            latency = Time.now - Time.at(timestamp)
+            puts "Latency: #{latency}s"
+          end
         end
       end
     end
